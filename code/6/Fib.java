7a6c065f3983f5c0a759ac3c8f391b41cd725edd
rolito <rolo.alv97@gmail.com>
2024-08-27 08:10:02 -0400

    6

---
+++ code/6/Fib.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Fib {
+
+  public static int fib(int n) {
+    if (n <= 1) {
+      return n;
+    } else {
+      return fib(n - 1) + fib(n - 2);
+    }
+  }
+
+  public static void main(String[] a) {
+    System.out.println(fib(1000));
+  }
+
+  // public static int fib(int n, int[] memo) {
+  //   if (n <= 1) {
+  //     return n;
+  //   } else if (memo[n] != 0) {
+  //     return memo[n];
+  //   } else {
+  //     memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  //     return memo[n];
+  //   }
+  // }
+}
